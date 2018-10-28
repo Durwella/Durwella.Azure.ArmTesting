7f9efda20d00a97a0d3abe6553acd4c9c6f5dd90
Jacob Foshee <jacobf@gmail.com>
2018-10-28 14:15:41 -0500

    Added new console project: Durwella.Azure.ArmTesting.Build Targeting both netcoreapp2.1 and net46

---
+++ Durwella.Azure.ArmTesting.Build/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Durwella.Azure.ArmTesting.Build
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Durwella.Azure.ArmTesting.Build!");
+        }
+    }
+}
