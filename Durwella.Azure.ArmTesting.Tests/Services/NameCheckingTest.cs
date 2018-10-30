671c63c4b204ab2e7df55b00923d8522cf3bb484
Jacob Foshee <jacobf@gmail.com>
2018-10-29 20:08:15 -0500

    Can get line numbers from JSON. Nice!

--- Durwella.Azure.ArmTesting.Tests/Services/NameCheckingTest.cs
+++ Durwella.Azure.ArmTesting.Tests/Services/NameCheckingTest.cs
@@ -1,5 +1,6 @@
 ﻿using Durwella.Azure.ArmTesting.Services;
 using FluentAssertions;
+using System.Linq;
 using Xunit;
 using static System.IO.File;
 using static System.IO.Path;
@@ -14,6 +15,22 @@ namespace Durwella.Azure.ArmTesting.Tests.Services
             var json = ReadAllText(Combine("Examples", "basic.json"));
             var errors = subject.CheckResourceNames(json);
             errors.Should().BeEmpty();
+        }
+
+        // Storage account name must be between 3 and 24 characters in length and use numbers and lower-case letters only.
+
+        [Theory(DisplayName = "Storage: long name"), AutoMoqData]
+        public void StorageNameTooLong(NameChecking subject)
+        {
+            var json = ReadAllText(Combine("Examples", "storage-basic.json"))
+                .Replace("storagename", "verylongstoragenamelongerthan24chars");
+
+            var errors = subject.CheckResourceNames(json);
+
+            errors.Should().HaveCount(1);
+            var error = errors.Single();
+            error.LineNumber.Should().Be(8);
+            error.Message.Should().Contain("too long");
         }
     }
 }
--- Durwella.Azure.ArmTesting/Services/NameChecking.cs
+++ Durwella.Azure.ArmTesting/Services/NameChecking.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace Durwella.Azure.ArmTesting.Services
 {
@@ -6,7 +8,23 @@ namespace Durwella.Azure.ArmTesting.Services
     {
         public IEnumerable<ArmTemplateError> CheckResourceNames(string armTemplateJson)
         {
-            return new List<ArmTemplateError>();
+            var loadSettings = new JsonLoadSettings
+            {
+                LineInfoHandling = LineInfoHandling.Load
+            };
+            var root = JObject.Parse(armTemplateJson, loadSettings);
+            var resources = root["resources"];
+            foreach (var resource in resources)
+            {
+                var name = resource["name"];
+                var lineInfo = (IJsonLineInfo)name;
+                yield return new ArmTemplateError(null, lineInfo.LineNumber, "The name is too long");
+            }
+
+
+            //return new List<ArmTemplateError>();
+            // 18:17:51 -     : zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz is not a valid storage account name. Storage account name must be between 3 and 24 characters in length and use numbers and lower-case letters only.
+
         }
     }
 }
