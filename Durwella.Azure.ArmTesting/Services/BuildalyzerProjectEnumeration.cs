346fa861df6debf56517ab2d3143eddc8da4eb02
Jacob Foshee <jacobf@gmail.com>
2018-10-30 21:13:04 -0500

    Targeting NetStandard2.0 so I can reference Buildalyzer. Sadly this breaks support for Net46.  I'll wait for demand to try to support Net46.

---
+++ Durwella.Azure.ArmTesting/Services/BuildalyzerProjectEnumeration.cs
@@ -0,0 +1,14 @@
+﻿using Durwella.Azure.ArmTesting.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Durwella.Azure.ArmTesting.Services
+{
+    public class BuildalyzerProjectEnumeration : IProjectFileEnumeration
+    {
+        public IEnumerable<string> EnumerateProjectFiles(string projectPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
