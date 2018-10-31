261ace1d8ae8279cbe27873a9839fb4a0e36c848
Jacob Foshee <jacobf@gmail.com>
2018-10-30 21:51:42 -0500

    Tests verifying BuildalyzerProjectEnumeration finds files in directory

---
+++ Durwella.Azure.ArmTesting.Tests/Services/BuildalyzerProjectEnumerationTest.cs
@@ -0,0 +1,112 @@
+﻿using Durwella.Azure.ArmTesting.Services;
+using FluentAssertions;
+using System.IO;
+using Xunit;
+using static Durwella.Azure.ArmTesting.Tests.Testing;
+using static System.IO.File;
+using static System.IO.Path;
+
+namespace Durwella.Azure.ArmTesting.Tests.Services
+{
+    public class BuildalyzerProjectEnumerationTest
+    {
+        [Theory(DisplayName = "Empty csproj"), AutoMoqData]
+        public void EmptyProject(BuildalyzerProjectEnumeration subject)
+        {
+            var directory = GetTemporaryDirectory();
+            var projectPath = CreateProjectFile(directory);
+
+            var filePaths = subject.EnumerateProjectFiles(projectPath);
+
+            filePaths.Should().BeEmpty();
+        }
+
+        [Theory(DisplayName = "main.cs"), AutoMoqData]
+        public void OneMainCs(BuildalyzerProjectEnumeration subject)
+        {
+            var directory = GetTemporaryDirectory();
+            var filePath = Create(directory, "main.cs");
+            var projectPath = CreateProjectFile(directory);
+
+            var filePaths = subject.EnumerateProjectFiles(projectPath);
+
+            filePaths.Should().Equal(filePath);
+        }
+
+        [Theory(DisplayName = "sample.json"), AutoMoqData]
+        public void OneSampleJson(BuildalyzerProjectEnumeration subject)
+        {
+            var directory = GetTemporaryDirectory();
+            var filePath = Create(directory, "sample.json");
+            var projectPath = CreateProjectFile(directory);
+
+            var filePaths = subject.EnumerateProjectFiles(projectPath);
+
+            filePaths.Should().Equal(filePath);
+        }
+
+        [Theory(DisplayName = "multiple in /"), AutoMoqData]
+        public void MultipleFilesInDirectory(BuildalyzerProjectEnumeration subject)
+        {
+            var directory = GetTemporaryDirectory();
+            string filePath1 = Create(directory, "file1.txt");
+            string filePath2 = Create(directory, "file2.cs");
+            string filePath3 = Create(directory, "file3.json");
+            var projectPath = CreateProjectFile(directory);
+
+            var filePaths = subject.EnumerateProjectFiles(projectPath);
+
+            filePaths.Should().BeEquivalentTo(filePath1, filePath2, filePath3);
+        }
+
+        [Theory(DisplayName = "multiple in /sub1 /sub2"), AutoMoqData]
+        public void MultipleFilesInSubdirectories(BuildalyzerProjectEnumeration subject)
+        {
+            var directory = GetTemporaryDirectory();
+            string filePath1 = Create(directory, "sub1", "file1.json");
+            string filePath2 = Create(directory, "sub2", "file2.cs");
+            var projectPath = CreateProjectFile(directory);
+
+            var filePaths = subject.EnumerateProjectFiles(projectPath);
+
+            filePaths.Should().BeEquivalentTo(filePath1, filePath2);
+        }
+
+        [Theory(DisplayName = "Not /bin/...")]
+        [InlineAutoMoqData("bin")]
+        [InlineAutoMoqData("obj")]
+        [InlineAutoMoqData(".vs")]
+        public void IgnoreBinDirectory(string binDir, BuildalyzerProjectEnumeration subject)
+        {
+            var directory = GetTemporaryDirectory();
+            Create(directory, binDir, "file.json");
+            var projectPath = CreateProjectFile(directory);
+
+            var filePaths = subject.EnumerateProjectFiles(projectPath);
+
+            filePaths.Should().BeEmpty();
+        }
+
+        private static string Create(string directory, string name)
+        {
+            var filePath = Combine(directory, name);
+            CreateText(filePath);
+            return filePath;
+        }
+
+        private static string Create(string directory, string subdirectory, string name)
+        {
+            Directory.CreateDirectory(Combine(directory, subdirectory));
+            var filePath = Combine(directory, subdirectory, name);
+            CreateText(filePath);
+            return filePath;
+        }
+
+        private static string CreateProjectFile(string directory)
+        {
+            var projectPath = Combine(directory, "Test.csproj");
+            WriteAllText(projectPath, @"<Project Sdk=""Microsoft.NET.Sdk""></Project>");
+            return projectPath;
+        }
+    }
+}
--- Durwella.Azure.ArmTesting/Services/BuildalyzerProjectEnumeration.cs
+++ Durwella.Azure.ArmTesting/Services/BuildalyzerProjectEnumeration.cs
@@ -1,14 +1,42 @@
-﻿using Durwella.Azure.ArmTesting.ServiceInterfaces;
-using System;
+﻿using Buildalyzer;
+using Durwella.Azure.ArmTesting.ServiceInterfaces;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Durwella.Azure.ArmTesting.Services
 {
     public class BuildalyzerProjectEnumeration : IProjectFileEnumeration
     {
+        private static readonly IList<string> InlcudedProjectKeys =
+            new[] { "None", "Compile", "Content", "EmbeddedResource" };
+
         public IEnumerable<string> EnumerateProjectFiles(string projectPath)
         {
-            throw new NotImplementedException();
+            var relativePaths = EnumerateProjectFilesRelative(projectPath);
+            var directory = Path.GetDirectoryName(projectPath);
+            return relativePaths.Select(relativePath => Path.Combine(directory, relativePath));
+        }
+
+        public IEnumerable<string> EnumerateProjectFilesRelative(string projectPath)
+        {
+            var manager = new AnalyzerManager();
+            var analyzer = manager.GetProject(projectPath);
+            var results = analyzer.Build();
+            var result = results.Single();
+            var items = result.Items;
+            foreach (var item in items)
+            {
+                // Skip keys like ProjectReference that aren't for files
+                if (!InlcudedProjectKeys.Contains(item.Key))
+                    continue;
+                var projectItems = item.Value;
+                foreach (var projectItem in projectItems)
+                {
+                    // The item spec for files will be the path relative to the project directory
+                    yield return projectItem.ItemSpec;
+                }
+            }
         }
     }
 }
