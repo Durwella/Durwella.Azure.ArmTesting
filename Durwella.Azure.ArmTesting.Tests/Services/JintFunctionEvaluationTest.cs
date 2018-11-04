fe0b9191839a869bc1199f898d73bb12795142aa
Jacob Foshee <jacobf@gmail.com>
2018-11-04 13:41:07 -0600

    Started implementation of ARM functions leveraging the Jint library. Started with concat().

---
+++ Durwella.Azure.ArmTesting.Tests/Services/JintFunctionEvaluationTest.cs
@@ -0,0 +1,22 @@
+﻿using Durwella.Azure.ArmTesting.Services;
+using FluentAssertions;
+using Xunit;
+
+namespace Durwella.Azure.ArmTesting.Tests.Services
+{
+    public class JintFunctionEvaluationTest
+    {
+        [Theory(DisplayName = "concat()")]
+        [InlineAutoMoqData("'a', 'b', 'c'", "abc")]
+        [InlineAutoMoqData("'el', 'ep', 'ha', 'nt'", "elephant")]
+        [InlineAutoMoqData("'vm', 2", "vm2")]
+        public void ConcatFunction(string concatArgs, string expected, JintFunctionEvaluation subject)
+        {
+            var expression = $"[concat({concatArgs})]";
+
+            var result = subject.Evaluate(expression);
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Durwella.Azure.ArmTesting/Services/JintFunctionEvaluation.cs
@@ -0,0 +1,20 @@
+﻿using Jint;
+
+namespace Durwella.Azure.ArmTesting.Services
+{
+    public class JintFunctionEvaluation
+    {
+        public string Evaluate(string armFunctionExpression)
+        {
+            var engine = new Engine();
+            engine.Execute(@"
+                function concat() {
+                  return ''.concat.apply('', arguments);
+                }
+            ");
+            var expression = armFunctionExpression.TrimStart('[').TrimEnd(']');
+            return engine.Execute(expression)
+                .GetCompletionValue().AsString();
+        }
+    }
+}
