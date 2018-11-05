3240752b966e6158bfa20c90a53b918a08a32dce
Jacob Foshee <jacobf@gmail.com>
2018-11-05 08:49:43 -0600

    WIP: Adding the `array()` function. Realizing I need to think about the ultimate return type for ARM function evaluation.

--- Durwella.Azure.ArmTesting.Tests/Services/JintFunctionEvaluationTest.cs
+++ Durwella.Azure.ArmTesting.Tests/Services/JintFunctionEvaluationTest.cs
@@ -6,6 +6,19 @@ namespace Durwella.Azure.ArmTesting.Tests.Services
 {
     public class JintFunctionEvaluationTest
     {
+        [Theory(DisplayName = "array()")]
+        [InlineAutoMoqData("1", "[1]")]
+        [InlineAutoMoqData("'efgh'", "['efgh']")]
+        [InlineAutoMoqData("{a: 'b', c: 'd'}", "[{a: 'b', c: 'd'}]")]
+        public void ArrayFunction(string argument, string expected, JintFunctionEvaluation subject)
+        {
+            var expression = $"[array({argument})]";
+
+            var result = subject.Evaluate(expression);
+
+            result.Should().Be(expected);
+        }
+
         [Theory(DisplayName = "concat()")]
         [InlineAutoMoqData("'a', 'b', 'c'", "abc")]
         [InlineAutoMoqData("'el', 'ep', 'ha', 'nt'", "elephant")]
--- Durwella.Azure.ArmTesting/Services/JintFunctionEvaluation.cs
+++ Durwella.Azure.ArmTesting/Services/JintFunctionEvaluation.cs
@@ -4,17 +4,22 @@ namespace Durwella.Azure.ArmTesting.Services
 {
     public class JintFunctionEvaluation
     {
-        public string Evaluate(string armFunctionExpression)
+        public object Evaluate(string armFunctionExpression)
         {
             var engine = new Engine();
             engine.Execute(@"
+                function array(arg) {
+                    return [arg];
+                }
+
                 function concat() {
                   return ''.concat.apply('', arguments);
                 }
             ");
             var expression = armFunctionExpression.TrimStart('[').TrimEnd(']');
             return engine.Execute(expression)
-                .GetCompletionValue().AsString();
+                .GetCompletionValue()
+                .AsString();
         }
     }
 }
